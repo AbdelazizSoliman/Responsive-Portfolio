74ec8a0824841e90f7adfd1fd4b4d4f2880d6a63
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-01-03 10:18:07 +0200

    Add js file

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
 // Hamburger menu
 const menuIcon = document.querySelector('.menu-icon');
 const navMenu = document.querySelector('.nav-items');
-navLinks= document.querySelectorAll('.nav-link');
-
+const navLinks = document.querySelectorAll('.nav-link');
 menuIcon.addEventListener('click', () => {
-        menuIcon.classList.toggle('active');
-        navMenu.classList.toggle('active');
+  menuIcon.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
 navLinks.forEach((navLink) => {
-     navLink.addEventListener('click', () => {
-        menuIcon.classList.remove('active');
-        navMenu.classList.remove('active');
-          });
-        });
-
+  navLink.addEventListener('click', () => {
+    menuIcon.classList.remove('active');
+    navMenu.classList.remove('active');
+  });
+});
